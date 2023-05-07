0425484ed80558248e773da8f5e818cc87bd74e5
kakashi <77133451+990052053@users.noreply.github.com>
2023-05-07 18:18:34 +0800

    Update customize3.sh

--- customize3.sh
+++ customize3.sh
@@ -11,10 +11,10 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 删除默认密码（默认无密码）
-# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/zzz-default-settings
+# sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/emortal/default-settings/files/zzz-default-settings
 
 # 取消bootstrap为默认主题
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
