5a6bfd49461ad3eef1738e12f324c2b71a5631e8
kakashi <77133451+990052053@users.noreply.github.com>
2023-05-07 17:14:31 +0800

    Create customize2.sh

---
+++ customize2.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 删除默认密码（默认无密码）
+# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/zzz-default-settings
+
+# 取消bootstrap为默认主题
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 更换linux内核
+# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
