e9e8464697d9d1fbf58eeba7de5ee22da1f45af0
kakashi <77133451+990052053@users.noreply.github.com>
2023-05-07 18:18:08 +0800

    Update customize2.sh

--- customize2.sh
+++ customize2.sh
@@ -6,10 +6,10 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 删除默认密码（默认无密码）
- sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+ sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # 取消bootstrap为默认主题
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
