d9aa70de2722de821459a34d116c48735155fa55
kakashi <77133451+990052053@users.noreply.github.com>
2023-05-09 13:32:59 +0800

    Update customize2.sh

--- customize2.sh
+++ customize2.sh
@@ -6,7 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 
-# lede
+# lean_lede
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.100/g' openwrt/package/base-files/files/bin/config_generate
