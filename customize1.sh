e13dbcc3f18586171adac596759ce68cd1fba3e8
kakashi <77133451+990052053@users.noreply.github.com>
2023-05-09 13:29:44 +0800

    Update customize1.sh

--- customize1.sh
+++ customize1.sh
@@ -9,7 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-#immortalwrt
+# immortalwrt
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.100/g' openwrt/package/base-files/files/bin/config_generate
