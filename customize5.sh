505ed9c2069c86a24313f054f2b3b1f7eb54eb0c
kakashi <77133451+990052053@users.noreply.github.com>
2023-06-03 22:53:02 +0800

    Update customize5.sh

--- customize5.sh
+++ customize5.sh
@@ -10,3 +10,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.100/g' openwrt/package/base-files/files/bin/config_generate
+
+# 2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/default-settings/files/zzz-default-settings
