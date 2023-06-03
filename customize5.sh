90ee8e228274b7670cb73ffc8fa76952fae17ca5
kakashi <77133451+990052053@users.noreply.github.com>
2023-06-03 22:41:33 +0800

    Update customize5.sh

--- customize5.sh
+++ customize5.sh
@@ -9,4 +9,4 @@
 # X-wrt
 
 # Modify default IP
-sed -i 's/192.168.15.1/192.168.2.100/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.100/g' openwrt/package/base-files/files/bin/config_generate
