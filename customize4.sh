262d097184e716b1b3343d0ca801afc48903e7c9
kakashi <77133451+990052053@users.noreply.github.com>
2024-02-06 15:15:35 +0800

    Create customize4.sh

---
+++ customize4.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+
+# LienolOP
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.100/g' openwrt/package/base-files/files/bin/config_generate
+
+# 2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/default-settings/files/zzz-default-settings
