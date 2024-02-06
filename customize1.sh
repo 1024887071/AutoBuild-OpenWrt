ddaa76ebfbbf30e47ad0e58349dc9c10014868d1
kakashi <77133451+990052053@users.noreply.github.com>
2024-02-06 15:51:32 +0800

    Update customize1.sh

--- customize1.sh
+++ customize1.sh
@@ -15,7 +15,7 @@
 sed -i 's/192.168.1.1/192.168.2.100/g' openwrt/package/base-files/files/bin/config_generate
 
 # 删除默认密码（默认无密码）
-# sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/emortal/default-settings/files/99-default-settings
+# sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # 取消bootstrap为默认主题
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
