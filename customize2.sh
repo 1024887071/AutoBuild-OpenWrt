f9267c9aa6242cd6f3339f4f9b2057907e2c5d9f
kakashi <77133451+990052053@users.noreply.github.com>
2024-02-06 15:39:06 +0800

    Update customize2.sh

--- customize2.sh
+++ customize2.sh
@@ -12,7 +12,7 @@
 sed -i 's/192.168.1.1/192.168.2.100/g' openwrt/package/base-files/files/bin/config_generate
 
 # 删除默认密码（默认无密码）
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
+# sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # 取消bootstrap为默认主题
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
