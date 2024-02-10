828de6b5cdbe4c5c349151bc2eff2ebddb16c890
kakashi <77133451+990052053@users.noreply.github.com>
2024-02-10 11:46:56 +0800

    Create customize5.sh

---
+++ customize5.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+
+# lean_lede
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.100/g' openwrt/package/base-files/files/bin/config_generate
+
+# 删除默认密码（默认无密码）
+# sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
+
+# 取消bootstrap为默认主题
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 更换linux内核
+# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
